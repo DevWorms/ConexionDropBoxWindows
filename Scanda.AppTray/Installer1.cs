b62fdf0df21b6b1746f705411fb8ab6bd65a696b
Paul Arizpe <kiramishima@outlook.com>
2016-09-22 01:31:16 -0500

    Correcion del uninstaller

---
+++ Scanda.AppTray/Installer1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scanda.AppTray
+{
+    [RunInstaller(true)]
+    public partial class Installer1 : System.Configuration.Install.Installer
+    {
+        public Installer1()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(IDictionary savedState)
+        {
+            base.Install(savedState);
+            //Add custom code here
+        }
+
+
+        public override void Rollback(IDictionary savedState)
+        {
+            base.Rollback(savedState);
+            //Add custom code here
+        }
+
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+            //Add custom code here
+        }
+
+        public override void Uninstall(IDictionary savedState)
+        {
+            Process application = null;
+            foreach (var process in Process.GetProcesses())
+            {
+                if (!process.ProcessName.ToLower().Contains("dbprotector")) continue;
+                application = process;
+                break;
+            }
+
+            if (application != null && application.Responding)
+            {
+                application.Kill();
+                base.Uninstall(savedState);
+            }
+        }
+    }
+}
